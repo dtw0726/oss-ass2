51a7d9364d4f949ffa183c0e0b7ce1892edd8ee2
dtw0726 <dtw0726@hansung.ac.kr>
2024-12-08 09:09:26 +0000

    H

--- oss-ass2/program.c
+++ oss-ass2/program.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
-int sum(int a, int b) {
-    return a + b;
-}
+int sum(int a, int b) { return a + b; }
 
 int main() {
     printf("Sum: %d\n", sum(3, 5));
     return 0;
 }
-
