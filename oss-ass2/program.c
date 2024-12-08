9abbc93cc8962a4afe2f50ca891a48cf8846c75a
dtw0726 <dtw0726@hansung.ac.kr>
2024-12-08 09:11:28 +0000

    I

--- oss-ass2/program.c
+++ oss-ass2/program.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int sum(int a, int b) {
-    return a + b;
+int fib(int n) {
+    if (n <= 1) return n;
+    return fib(n-1) + fib(n-2);
 }
 
 int main() {
-    printf("Sum: %d\n", sum(3, 5));
+    printf("Fib: %d\n", fib(5));
     return 0;
 }
-
